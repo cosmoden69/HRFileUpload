1c98d6e93cb92f47452a01c9c2245d3a9d487511
cosmoden69 <cosmoden@hotmail.com>
2022-04-27 14:47:37 +0900

    초기 커밋.

---
+++ frmFileUpload.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace HRFileUpload
+{
+    partial class frmFileUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnUploadStart = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotalCnt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.txtUpMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUpCnt = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnUploadStart);
+            this.panel1.Controls.Add(this.txtFolderPath);
+            this.panel1.Controls.Add(this.btnOpenFolder);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtTotalCnt);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.txtUpMsg);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtUpCnt);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 289);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnUploadStart
+            // 
+            this.btnUploadStart.Location = new System.Drawing.Point(99, 62);
+            this.btnUploadStart.Name = "btnUploadStart";
+            this.btnUploadStart.Size = new System.Drawing.Size(110, 32);
+            this.btnUploadStart.TabIndex = 33;
+            this.btnUploadStart.Text = "업로드 시작";
+            this.btnUploadStart.UseVisualStyleBackColor = true;
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderPath.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFolderPath.Location = new System.Drawing.Point(99, 24);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(673, 21);
+            this.txtFolderPath.TabIndex = 32;
+            this.txtFolderPath.TabStop = false;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(24, 22);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(69, 23);
+            this.btnOpenFolder.TabIndex = 31;
+            this.btnOpenFolder.Text = "폴더선택";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "전체파일건수";
+            // 
+            // txtTotalCnt
+            // 
+            this.txtTotalCnt.Location = new System.Drawing.Point(117, 132);
+            this.txtTotalCnt.Name = "txtTotalCnt";
+            this.txtTotalCnt.ReadOnly = true;
+            this.txtTotalCnt.Size = new System.Drawing.Size(116, 21);
+            this.txtTotalCnt.TabIndex = 29;
+            this.txtTotalCnt.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 205);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(778, 1);
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 115);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(778, 1);
+            this.pictureBox5.TabIndex = 27;
+            this.pictureBox5.TabStop = false;
+            // 
+            // txtUpMsg
+            // 
+            this.txtUpMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUpMsg.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUpMsg.Location = new System.Drawing.Point(24, 168);
+            this.txtUpMsg.Name = "txtUpMsg";
+            this.txtUpMsg.ReadOnly = true;
+            this.txtUpMsg.Size = new System.Drawing.Size(748, 21);
+            this.txtUpMsg.TabIndex = 16;
+            this.txtUpMsg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "진행건수";
+            // 
+            // txtUpCnt
+            // 
+            this.txtUpCnt.Location = new System.Drawing.Point(311, 132);
+            this.txtUpCnt.Name = "txtUpCnt";
+            this.txtUpCnt.ReadOnly = true;
+            this.txtUpCnt.Size = new System.Drawing.Size(116, 21);
+            this.txtUpCnt.TabIndex = 9;
+            this.txtUpCnt.TabStop = false;
+            // 
+            // frmFileUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 289);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmFileUpload";
+            this.Text = "인사 첨부파일 업로드";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUpMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUpCnt;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotalCnt;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnUploadStart;
+    }
+}
---
+++ frmFileUpload.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YLWService;
+
+namespace HRFileUpload
+{
+    public partial class frmFileUpload : Form
+    {
+        public frmFileUpload()
+        {
+            InitializeComponent();
+
+            btnOpenFolder.Click += BtnOpenFolder_Click;
+            btnUploadStart.Click += BtnUploadStart_Click;
+
+            string userRoot = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            txtFolderPath.Text = Path.Combine(userRoot, "Downloads", "Upload");
+        }
+
+        private void ClearSceen()
+        {
+            txtTotalCnt.Text = "";
+            txtUpCnt.Text = "";
+            txtUpMsg.Text = "";
+        }
+
+        private void BtnOpenFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+                dlg.SelectedPath = txtFolderPath.Text;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolderPath.Text = dlg.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnUploadStart_Click(object sender, EventArgs e)
+        {
+            string path = txtFolderPath.Text;
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("업로드할 파일이 있는 폴더를 선택하세요");
+                return;
+            }
+
+            try
+            {
+                btnUploadStart.Enabled = false;
+                Cursor = Cursors.WaitCursor;
+
+                var files = Directory.GetFiles(path, "*.*");
+                txtTotalCnt.Text = Utils.ConvertToString(files?.Length);
+                txtUpCnt.Text = "";
+                Application.DoEvents();
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        txtUpMsg.Text = "Read File : " + Path.GetFileName(file);
+                        Application.DoEvents();
+
+                        //압축파일이면 따로 처리
+                        string ext = Path.GetExtension(file);
+                        if (ext.ToUpper() == ".ZIP")
+                        {
+                            if (fnUploadZipFile(file) == false) continue;  //압축파일 업로드 실패
+
+                            File.Delete(file);
+                            txtUpCnt.Text = Utils.ConvertToString(Utils.ToInt(txtUpCnt.Text) + 1);
+                            Application.DoEvents();
+                            continue;
+                        }
+
+                        string filename = Path.GetFileNameWithoutExtension(file);
+                        string empid = Utils.GetP(filename, "_", 1);
+                        string ftype = Utils.GetP(filename, "_", 2);
+                        string upname = Utils.GetP(filename, "_", 3);
+                        if (fnFileUpload(file, empid, ftype, upname) == false) continue;
+
+                        File.Delete(file);
+                        txtUpCnt.Text = Utils.ConvertToString(Utils.ToInt(txtUpCnt.Text) + 1);
+                        Application.DoEvents();
+                    }
+                    catch (Exception ex)
+                    {
+                        txtUpMsg.Text = ex.Message;
+                        YLWService.LogWriter.WriteLog(ex.Message);
+                        string filepath = Path.GetDirectoryName(file);
+                        filepath = Path.Combine(filepath, "Error_backup");
+                        if (!Directory.Exists(filepath)) Directory.CreateDirectory(filepath);
+                        string tofile = Path.Combine(filepath, Path.GetFileName(file));
+                        FileMoveTo(file, filepath);
+                        Application.DoEvents();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+                btnUploadStart.Enabled = true;
+            }
+        }
+
+        private bool fnUploadZipFile(string zipFile)
+        {
+            try
+            {
+                string zipPath = Path.GetDirectoryName(zipFile);
+                string zipFilename = Path.GetFileNameWithoutExtension(zipFile);
+                zipPath = Path.Combine(zipPath, zipFilename);
+                if (!Directory.Exists(zipPath)) Directory.CreateDirectory(zipPath);
+                Utils.ClearFolder(zipPath);
+                Utils.ZipExtract(zipFile, zipPath);
+                var files = Directory.GetFiles(zipPath, "*.*");
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        string ftype = zipFilename;
+                        string filename = Path.GetFileNameWithoutExtension(file);
+                        string empid = Utils.GetP(filename, "_", 1);
+                        string upname = Utils.GetP(filename, "_", 2);
+                        if (fnFileUpload(file, empid, ftype, upname) == false) continue;
+
+                        File.Delete(file);
+                        txtUpCnt.Text = Utils.ConvertToString(Utils.ToInt(txtUpCnt.Text) + 1);
+                        Application.DoEvents();
+                    }
+                    catch (Exception ex)
+                    {
+                        txtUpMsg.Text = ex.Message;
+                        YLWService.LogWriter.WriteLog(ex.Message);
+                        string filepath = Path.GetDirectoryName(file);
+                        filepath = Path.Combine(filepath, "Error_backup");
+                        if (!Directory.Exists(filepath)) Directory.CreateDirectory(filepath);
+                        string tofile = Path.Combine(filepath, Path.GetFileName(file));
+                        FileMoveTo(file, filepath);
+                        Application.DoEvents();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool fnFileUpload(string file, string empid, string ftype, string upname)
+        {
+            try
+            {
+                if (empid == "") return false;
+                string empSeq = fnGetEmpSeq(empid);
+                if (empSeq == "") return false;
+
+                string fileConstSeq = "1";
+                string fileSeq = "0";
+                string idxNo = "1";
+                switch (ftype.ToUpper())
+                {
+                    case "주민등록등본":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "1;0;0;0;0;0;0;0;0");
+                        break;
+                    case "통장사본":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "0;1;0;0;0;0;0;0;0");
+                        break;
+                    case "차량유지비":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;1;0;0;0;0;0;0");
+                        break;
+                    case "육아수당":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;1;0;0;0;0;0");
+                        break;
+                    case "졸업증명":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;0;1;0;0;0;0");
+                        break;
+                    case "성적증명":
+                        fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;0;0;1;0;0;0");
+                        break;
+                    case "경력":
+                        //fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;0;0;0;1;0;0");
+                        break;
+                    case "자격면허":
+                        //fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;0;0;0;0;1;0");
+                        break;
+                    case "어학시험":
+                        //fileSeq = fnGetFileSeq(ftype, empSeq, "0;0;0;0;0;0;0;0;1");
+                        break;
+                    default:
+                        return false;
+                }
+                fileSeq = fnFileUpload(fileConstSeq, fileSeq, idxNo, upname, file);
+                if (!fnSaveHRTables(ftype, empSeq, fileSeq, upname)) return false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string fnGetEmpSeq(string empId)
+        {
+            try
+            {
+                YLWService.YlwSecurityJson security = YLWService.MTRServiceModule.SecurityJson.Clone();  //깊은복사
+
+                string strSql = "";
+                strSql += " SELECT emp.EmpSeq, emp.EmpName ";
+                strSql += " FROM   _TDAEmp AS emp WITH(NOLOCK) ";
+                strSql += " WHERE  emp.CompanySeq = '" +  Utils.ConvertToString(security.companySeq) + "' ";
+                strSql += " AND    emp.Empid = '" + empId + "' ";
+                strSql += " FOR JSON PATH ";
+                DataTable dtr = YLWService.MTRServiceModule.GetMTRServiceDataTable(security.companySeq, strSql);
+                if (dtr != null && dtr.Rows.Count > 0) return Utils.ConvertToString(dtr.Rows[0]["EmpSeq"]);
+                throw new Exception("사번[" + empId + "]의 EmpSeq를 찾을 수 없습니다");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string fnGetFileSeq(string fileType, string empSeq, string fileSelect)
+        {
+            try
+            {
+                YLWService.YlwSecurityJson security = YLWService.MTRServiceModule.SecurityJson.Clone();  //깊은복사
+                security.serviceId = "Metro.Package.AdjHR.BisEmpAttachMng";
+                security.methodId = "QueryFile";
+
+                DataSet ds = new DataSet("ROOT");
+                DataTable dt = ds.Tables.Add("DataBlock1");
+                dt.Columns.Add("EmpSeq");
+                dt.Columns.Add("FileSelect");
+                dt.Clear();
+                DataRow dr = dt.Rows.Add();
+                dr["EmpSeq"] = empSeq;
+                dr["FileSelect"] = fileSelect;
+
+                DataSet yds = YLWService.MTRServiceModule.CallMTRServiceCallPost(security, ds);
+                if (yds == null || yds.Tables.Count < 1) return "0";
+                DataTable ydt = yds.Tables[0];
+                if (ydt == null || ydt.Rows.Count < 1) return "0";
+                if (fileType != ydt.Rows[0]["FileType"] + "") return "0";
+                return ydt.Rows[0]["FileSeq"] + "";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string fnFileUpload(string fileConstSeq, string fileSeq, string idxNo, string upname, string file)
+        {
+            try
+            {
+                string filepath = Path.GetDirectoryName(file);
+                filepath = Path.Combine(filepath, "Temp");
+                if (!Directory.Exists(filepath)) Directory.CreateDirectory(filepath);
+                Utils.ClearFolder(filepath);
+                string tofile = Path.Combine(filepath, upname + Path.GetExtension(file));
+                File.Copy(file, tofile);
+
+                YlwSecurityJson security = YLWService.MTRServiceModule.SecurityJson.Clone();  //깊은복사
+                // File Info
+                FileInfo finfo = new FileInfo(tofile);
+                byte[] rptbyte = (byte[])MetroSoft.HIS.cFile.ReadBinaryFile(tofile);
+                string fileBase64 = Convert.ToBase64String(rptbyte);
+                fileSeq = YLWService.MTRServiceModule.CallMTRFileuploadGetSeq(security, finfo, fileBase64, fileConstSeq, fileSeq, "0", idxNo);
+                if (fileSeq != "") return fileSeq;
+                throw new Exception("FileUpload : file_seq not returned");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool fnSaveHRTables(string fileType, string empSeq, string fileSeq, string upname)
+        {
+            try
+            {
+                YLWService.YlwSecurityJson security = YLWService.MTRServiceModule.SecurityJson.Clone();  //깊은복사
+                security.serviceId = "Metro.Package.AdjHR.BisEmpAttachMng";
+                security.methodId = "FileUpload";
+
+                DataSet ds = new DataSet("ROOT");
+                DataTable dt = ds.Tables.Add("DataBlock1");
+                dt.Columns.Add("EmpSeq");
+                dt.Columns.Add("DeptSeq");
+                dt.Columns.Add("FileSelect");
+                dt.Columns.Add("FileType");
+                dt.Columns.Add("FileSeq");
+                dt.Columns.Add("FileName");
+
+                dt.Clear();
+                DataRow dr = dt.Rows.Add();
+                dr["EmpSeq"] = empSeq;
+                dr["FileType"] = fileType;
+                dr["FileSeq"] = fileSeq;
+                dr["FileName"] = upname;
+
+                DataSet yds = YLWService.MTRServiceModule.CallMTRServiceCallPost(security, ds);
+                if (yds == null)
+                {
+                    return false;
+                }
+                foreach (DataTable dti in yds.Tables)
+                {
+                    if (!dti.Columns.Contains("Status")) continue;
+                    if (!dti.Columns.Contains("Result")) continue;
+                    if (dti.Rows.Count > 0 && Convert.ToInt32(dti.Rows[0]["Status"]) != 0)   //Status != 0 이면 저장안됨
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private string fnGetFileName(string downloadFolder, string fileName)
+        {
+            if (!Directory.Exists(downloadFolder)) Directory.CreateDirectory(downloadFolder);
+            string file = Path.Combine(downloadFolder, fileName);
+            int fileCount = 0;
+            while (File.Exists(file))
+            {
+                fileCount++;
+                file = Path.GetFileNameWithoutExtension(fileName) + "(" + fileCount.ToString() + ")" + Path.GetExtension(fileName);
+                file = Path.Combine(downloadFolder, file);
+            }
+            return file;
+        }
+
+        private bool FileMoveTo(string sourceFile, string destPath)
+        {
+            try
+            {
+                string filename = Path.GetFileName(sourceFile);
+                string destFile = fnGetFileName(destPath, filename);
+                File.Move(sourceFile, destFile);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
